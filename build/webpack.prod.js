26696478e44a091e4bcf52cd2870b987389a34e9
jaryal-anuj <jariyal.anuj@gmail.com>
2020-05-06 02:35:40 -0700

    uglifying and minifying

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,26 @@
 const base = require('./webpack.base');
 const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 
-module.exports = merge(base,{});
+module.exports = merge(base,{
+    plugins:[
+        new UglifyJSPlugin()
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.scss$/,
+                use : [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options:{
+                            minimize:true
+                        }
+                    },
+                    'sass-loader']
+            },
+        ]
+    }
+});
