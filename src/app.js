77a9d0efd171e1fe020d50d5ef22b640fa0d7c06
jaryal-anuj <jariyal.anuj@gmail.com>
2020-05-07 03:52:20 -0700

    es-let and var

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-import 'styles/app.scss';
-import * as api from '@/app/api';
 
-api.getPosts().then(response=>{
-    console.log(response);
-})
+let messages = ["Hi there", "I'm good thanks", "How are you"];
+
+for(let i=0;i<messages.length;i++){
+    setTimeout(function() {
+        console.log(messages[i]);
+    }, i*(Math.floor(Math.random()*2500+1500)));
+}
