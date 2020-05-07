c8ad6094b488813edf4070c646c5e5ed89e652c3
jaryal-anuj <jariyal.anuj@gmail.com>
2020-05-07 05:04:38 -0700

    tagged template literals

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
+var emnum = (strings, ...keys)=>{
+    console.log(strings)
+    console.log(keys)
+    keys.forEach((value, index)=>{
+        if(typeof value == 'number'){
+            keys[index] = `<em>${value}</em>`
+        }
+    })
 
-var register=(email,username, password )=>{
-    console.log("Registered",email,username,password);
+    var result = strings[0];
+    for(let i=0;i<keys.length;i++){
+        result +=keys[i]+strings[i+1]
+    }
+
+    return result
 }
 
-var payload = ['alex@codecourse.com','alex','cats'];
+var name = 'Alex';
+var messagecount =20
 
-register.apply(null,payload)
-register(...payload)
+var greeting = emnum`Hi, ${name} you have ${messagecount} messages`;
+
+console.log(greeting);
